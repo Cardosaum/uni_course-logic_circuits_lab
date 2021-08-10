4fc8757d66864ab30799ce18134d7495722bc671
Cardosaum <cardosaum@pm.me>
2021-08-10 19:46:24 -0300

    firs version - Exp1

--- Exp01/.auctex-auto/relatorio.el
+++ Exp01/.auctex-auto/relatorio.el
@@ -1,10 +1,57 @@
 (TeX-add-style-hook
  "relatorio"
  (lambda ()
-   (LaTeX-add-bibitems
-    "knuth:84"
-    "boulic:91"
-    "smith:99"
-    "systemverilog"))
- :bibtex)
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "12pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("babel" "brazil" "american") ("inputenc" "utf8")))
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art12"
+    "sbc-template"
+    "babel"
+    "inputenc"
+    "graphicx"
+    "url"
+    "float"
+    "listings"
+    "color"
+    "todonotes"
+    "algorithmic"
+    "algorithm"
+    "hyperref"
+    "amsmath"
+    "array")
+   (LaTeX-add-labels
+    "sec:Introducao"
+    "sec:Objetivos"
+    "sec:Materiais"
+    "sec:Procedimentos"
+    "sec:led_chave"
+    "fig:exp1_2.1_f1"
+    "fig:exp1_2.1_f2"
+    "sec:and_e_or"
+    "tab:tabela_and"
+    "tab:tabela_or"
+    "sec:and_with_only_or_and_not"
+    "fig:exp1_2.3_f1"
+    "eq:and1"
+    "sec:Resultados"
+    "sec:Conclusao"
+    "fig:exp1_4.3_f1"
+    "tab:tabela_verdade")
+   (LaTeX-add-bibliographies))
+ :latex)
 
