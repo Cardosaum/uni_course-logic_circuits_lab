8d482512332d1d47012720580658fad4be1f32f6
Cardosaum <cardosaum@pm.me>
2021-08-28 00:06:24 -0300

    finish Exp3

---
+++ Exp03/.auctex-auto/ualiton.el
@@ -0,0 +1,65 @@
+(TeX-add-style-hook
+ "ualiton"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "12pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("babel" "brazil" "american") ("inputenc" "utf8")))
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art12"
+    "sbc-template"
+    "babel"
+    "inputenc"
+    "graphicx"
+    "url"
+    "float"
+    "listings"
+    "color"
+    "todonotes"
+    "algorithmic"
+    "algorithm"
+    "hyperref"
+    "amsmath"
+    "array")
+   (LaTeX-add-labels
+    "sec:Introducao"
+    "sec:Objetivos"
+    "sec:Materiais"
+    "sec:Procedimentos"
+    "sec:decisao_maioria"
+    "tab:tabela_and"
+    "fig:2_1_karnaugh_map.png"
+    "eq:2.1_NAND"
+    "fig:decisao_maioria"
+    "fig:decisao_maioria_montagem"
+    "sec:decisao_minoria"
+    "tab:decisao_minoria"
+    "fig:2_2_karnaugh_map.png"
+    "eq:2.2_NAND"
+    "fig:decisao_minoria"
+    "fig:decisao_minoria_montagem"
+    "sec:decisao_igualdade"
+    "tab:decisao_igualdade"
+    "tab:tabela_verdade_decisao_igualdade"
+    "eq:2.3_NAND"
+    "fig:decisao_igualdade"
+    "fig:decisao_igualdade_montagem"
+    "sec:Resultados"
+    "sec:Conclusao")
+   (LaTeX-add-bibliographies
+    "relatorio"))
+ :latex)
+
