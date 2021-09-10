659e89b4ccb3b53165fec1a2c219e2a506bc6822
Cardosaum <cardosaum@pm.me>
2021-09-10 12:33:23 -0300

    merge more Ualiton additions

--- Exp05/.auctex-auto/relatorio.el
+++ Exp05/.auctex-auto/relatorio.el
@@ -1,55 +1,8 @@
 (TeX-add-style-hook
  "relatorio"
  (lambda ()
-   (TeX-add-to-alist 'LaTeX-provided-class-options
-                     '(("article" "12pt")))
-   (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("babel" "brazil" "american") ("inputenc" "utf8") ("enumitem" "shortlabels")))
-   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
-   (TeX-run-style-hooks
-    "latex2e"
-    "article"
-    "art12"
-    "sbc-template"
-    "babel"
-    "inputenc"
-    "graphicx"
-    "url"
-    "float"
-    "listings"
-    "color"
-    "todonotes"
-    "algorithmic"
-    "algorithm"
-    "hyperref"
-    "amsmath"
-    "array"
-    "enumitem")
-   (LaTeX-add-labels
-    "sec:Introducao"
-    "sec:Objetivos"
-    "sec:Materiais"
-    "sec:Procedimentos"
-    "sec:elaboração_codificador"
-    "tab:comparador_de_palavras_3_bits"
-    "fig:galaxy"
-    "sec:encoder_simulations"
-    "sec:boolean_functions_decoder"
-    "tab:tabela_and"
-    "sec:todooo"
-    "sec:ham"
-    "sec:Resultados"
-    "sec:Conclusao")
-   (LaTeX-add-bibliographies))
- :latex)
+   (LaTeX-add-bibitems
+    "lcl"
+    "codificadores_mandelli"))
+ :bibtex)
 
