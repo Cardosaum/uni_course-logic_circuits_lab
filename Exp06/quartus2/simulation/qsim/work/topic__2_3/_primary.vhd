34ffd5b73ace29ec5e009d59c30bbd0cd1bd9112
uventura <ualivent@gmail.com>
2021-10-08 15:31:26 -0300

    Exp06 - Ualiton Feelings

---
+++ Exp06/quartus2/simulation/qsim/work/topic__2_3/_primary.vhd
@@ -0,0 +1,14 @@
+library verilog;
+use verilog.vl_types.all;
+entity \topic__2_3\ is
+    port(
+        \Out\           : out    vl_logic;
+        F               : in     vl_logic;
+        E               : in     vl_logic;
+        G               : in     vl_logic;
+        A               : in     vl_logic;
+        B               : in     vl_logic;
+        C               : in     vl_logic;
+        D               : in     vl_logic
+    );
+end \topic__2_3\;
