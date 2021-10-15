8fb9102be53a8c98549496b042fb87cb1c6d078a
Cardosaum <cardosaum@pm.me>
2021-10-15 19:47:23 -0300

    update text

--- Exp07/.auctex-auto/relatorio.el
+++ Exp07/.auctex-auto/relatorio.el
@@ -6,16 +6,16 @@
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("babel" "brazil" "american") ("inputenc" "utf8") ("enumitem" "shortlabels")))
    (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
    (TeX-run-style-hooks
     "latex2e"
     "article"
@@ -48,9 +48,13 @@
     "sec:2.1"
     "fig:diagram_2.1.png"
     "tab:truth_table_latch_rs"
+    "fig:Circ-2.1.png"
     "sec:2.2"
     "fig:LatchRS-Engatilhado.png"
+    "tab:truth_table_latch_rs_triggered"
     "sec:2.3"
+    "fig:Flip-Flop-RS.png"
+    "tab:truth_table_flipflop_rs"
     "sec:2.4"
     "sec:resultados"
     "sec:analise2.1"
