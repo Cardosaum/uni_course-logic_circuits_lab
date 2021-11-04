6a3a60f412213f161cb4e5dd013300ba060425e2
Cardosaum <cardosaum@pm.me>
2021-11-03 21:51:39 -0300

    write scafold for final report

---
+++ Projeto/.auctex-auto/relatorio.el
@@ -0,0 +1,67 @@
+(TeX-add-style-hook
+ "relatorio"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "12pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("babel" "brazil" "american") ("inputenc" "utf8") ("enumitem" "shortlabels") ("ifsym" "electronic")))
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art12"
+    "sbc-template"
+    "babel"
+    "inputenc"
+    "graphicx"
+    "url"
+    "float"
+    "listings"
+    "color"
+    "todonotes"
+    "algorithmic"
+    "algorithm"
+    "hyperref"
+    "amsmath"
+    "array"
+    "mwe"
+    "enumitem"
+    "xcolor"
+    "ifsym"
+    "trace")
+   (TeX-add-symbols
+    "colorIndex")
+   (LaTeX-add-labels
+    "sec:Introducao"
+    "sec:Objetivos"
+    "sec:Materiais"
+    "sec:Procedimentos"
+    "sec:2.1"
+    "sec:2.2"
+    "sec:2.3"
+    "sec:2.4"
+    "sec:resultados"
+    "sec:analise2.1"
+    "sec:analise2.2"
+    "sec:analise2.3"
+    "sec:analise2.4"
+    "sec:Conclusao")
+   (LaTeX-add-bibliographies)
+   (LaTeX-add-listings-lstdefinestyles
+    "verilog-style")
+   (LaTeX-add-xcolor-definecolors
+    "vgreen"
+    "vblue"
+    "vorange"))
+ :latex)
+
